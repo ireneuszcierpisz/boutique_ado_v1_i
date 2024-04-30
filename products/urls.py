8ee24b3b13b9524de8a4e0829d28666289d936f7
Irek Cierpisz <ireneuszcierpisz@gmail.com>
2024-04-30 12:14:41 +0000

    Add search functionality

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # the 'products' url returns all_products view
     path('', views.all_products, name='products'),
     # the url contain the product ID, returns the product_detail view and is named 'product_detail':
     path('<product_id>', views.product_detail, name='product_detail'),
--- products/views.py
+++ products/views.py
@@ -1,17 +1,36 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+# a special object from Jango.db.models called Q to generate a search query:
+from django.db.models import Q
 from .models import Product
 
-# Create your views here.
 # define an all_products view which will render the products template
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     # return all products from the database
     products = Product.objects.all()
+    query = None
+
+    # when a search query is submited it end up in the url as a GET parameter.
+    # We can access those url parameter in the all_products view by checking whether request.get exists:
+    if request.GET:
+        # Since we named the text input in the form in base.html 'q', we can check if 'q' is in request.GET
+        if 'q' in request.GET:
+            query = request.GET['q']
+            #If the query is blank it's not going to return any results 
+            if not query:
+                # we use the Django messages framework to attach an error message to the request
+                messages.error(request, "You didn't enter any search criteria")
+                # and redirect back to the products url
+                return redirect(reverse('products'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     # add all products to the context so products will be available in the template
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
