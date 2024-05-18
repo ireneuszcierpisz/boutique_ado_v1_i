bb2e3d2ce89739e045ea44169d38161b559f61b0
Irek Cierpisz <ireneuszcierpisz@gmail.com>
2024-05-18 22:14:55 +0000

    Add adjust bag, remove from bag and calculate subtotal template filter.

---
+++ bag/templatetags/bag_tools.py
@@ -0,0 +1,20 @@
+"""
+Write a custom template filter to calculate the subtotal value in the bag;
+For the subtotal column, the subtotal should be the quantity times the product price
+"""
+
+# as there is the empty file called __init__.py in templatetags folder
+# so that this directory is treated as a Python package
+# making bag_tools.py module available for imports and to use in templates
+from django import template
+
+# create a variable register which is an instance of template library
+register = template.Library()
+
+# use the register filter decorator to register  function calc_subtotal as a template filter
+# (django doc: creating custom template tags and filters)
+@register.filter(name='calc_subtotal')
+
+# define the filter which returns a product of price and quantity
+def calc_subtotal(price, quantity):
+    return price * quantity
